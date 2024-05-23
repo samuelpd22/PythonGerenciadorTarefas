d1163d26820d78b4c6dc694b9cd4abddd7170a7b
samuelpd22 <samuel050101@hotmail.com>
2024-05-23 17:44:03 -0300

    finally

--- app.py
+++ app.py
@@ -19,3 +19,10 @@ while True :
     event,values = janela.read()
     if event == sg.WIN_CLOSED:
         break
+    elif event == 'Nova Tarefa':
+        janela.extend_layout(janela['container'],
+       [[sg.Checkbox(''),
+         sg.Input('')]])
+    elif event == 'Resetar':
+        janela.Close(),
+        janela = criar_nova_tarefa()
