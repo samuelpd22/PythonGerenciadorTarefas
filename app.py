1b19325b57b776765cfa332030897add23f0a67b
samuelpd22 <samuel050101@hotmail.com>
2024-05-23 17:37:42 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import PySimpleGUI as sg
+
+def criar_nova_tarefa():
+    sg.theme('DarkBlue4')
+    linha = [
+        [sg.Checkbox(''),sg.Input('')],
+        
+    ],
+    layout = [
+        [sg.Frame('Tarefas',layout=linha, key='container')],
+        [sg.Button('Nova Tarefa'),sg.Button('Resetar')]
+    ]
+
+    return sg.Window('Todo List',layout=layout, finalize=True)
+#Criar janela
+janela = criar_nova_tarefa()
+#Criar regras da janela
+while True :
+    event,values = janela.read()
+    if event == sg.WIN_CLOSED:
+        break
